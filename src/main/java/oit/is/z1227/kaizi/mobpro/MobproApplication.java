69a072f470117c5c345825fc24d87799d99e8232
kuribayashi1026 <e1b18032@st.oit.ac.jp>
2020-10-06 15:49:53 +0900

    初期化

---
+++ src/main/java/oit/is/z1227/kaizi/mobpro/MobproApplication.java
@@ -0,0 +1,13 @@
+package oit.is.z1227.kaizi.mobpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z1227/kaizi/mobpro/MobproApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z1227.kaizi.mobpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
